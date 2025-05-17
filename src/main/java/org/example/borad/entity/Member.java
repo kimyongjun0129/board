af920a377daac708aa05bb79f4d0f4f382fbd778
kimyongjun0129 <kimyongjun0129@naver.com>
2025-05-17 20:19:09 +0900

    feat ; 기본 파일 생성 및 Member Entity 생성

---
+++ src/main/java/org/example/borad/entity/Member.java
@@ -0,0 +1,19 @@
+package org.example.borad.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "member")
+public class Member extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private Integer age;
+}
