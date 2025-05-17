8bf139323c1178e81e28a5c367bf219e2bba18b6
kimyongjun0129 <kimyongjun0129@naver.com>
2025-05-17 20:06:29 +0900

    feat : first commit

---
+++ src/test/java/org/example/borad/BoradApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.borad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoradApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
