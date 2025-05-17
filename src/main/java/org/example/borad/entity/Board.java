34b86bdd7600fc5de2f8c7b4658fcd853b2fd53e
kimyongjun0129 <kimyongjun0129@naver.com>
2025-05-17 20:22:05 +0900

    feat : Borad Entity 생성

---
+++ src/main/java/org/example/borad/entity/Board.java
@@ -0,0 +1,18 @@
+package org.example.borad.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "borad")
+public class Board extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "longtext")
+    private String content;
+}
